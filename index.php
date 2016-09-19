1daf241418af36b51e1235992b88d3af72cbe470
Michael Hall <hall@michaelhall.se>
2016-09-19 21:32:17 +0200

    Remove space in string concatenation

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ use BlueMvc\Core\Application;
 use BlueMvc\Core\Request;
 use BlueMvc\Core\Response;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 $application = new Application();
-require_once __DIR__ . '/setup.php';
+require_once __DIR__.'/setup.php';
 
 $request = new Request();
 $response = new Response($request);
